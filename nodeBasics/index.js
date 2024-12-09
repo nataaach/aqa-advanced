20a3715765d455835e96b36814aa38afaa580a5d
nataaach <nataliacervakova0@gmail.com>
2024-12-09 13:50:42 +0200

    домашка2

---
+++ nodeBasics/index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+
+console.log(chalk.green('Це'));
+console.log(chalk.blue('моя'));
+console.log(chalk.red('домашка'));
+console.log(chalk.bold.underline.yellow('#2'));
+
