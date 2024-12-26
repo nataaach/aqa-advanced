1ce5416fa9a1eb69b90153bf0c9da97fd7d326f4
nataaach <nataliacervakova0@gmail.com>
2024-12-26 10:58:08 +0200

    .

--- homeworks/task3.js
+++ homeworks/task3.js
@@ -2,9 +2,9 @@ function checkOrder(available, ordered) {
     if (ordered > available) {
         return "Your order is too large, we don’t have enough goods.";
     } else if (ordered === 0) {
-        return "Your order is empty";
+        return "Your order is empty.";
     } else {
-        return "Your order is accepted";
+        return "Your order is accepted.";
     }
 }
 
